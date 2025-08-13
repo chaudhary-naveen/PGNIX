02fd196e938a3569be0136b9cfd5799f9df4ddaa
anshkadiyan-reddoorz <ansh.kadiyan@reddoorz.com>
2025-08-13 16:17:04 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-<<<<<<< HEAD
 import {
   Routes,
   Route,
@@ -10,19 +9,14 @@ import {
 
 import HomePage from './HomePage/HomePage';
 
-=======
 import ProductPage from './productPage/ProductPage'
->>>>>>> 748145c (product Page is created)
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <Routes>
         <Route path='/' element={<HomePage/>} ></Route>
       </Routes>
-=======
      <ProductPage/>
->>>>>>> 748145c (product Page is created)
     </>
   )
 }
