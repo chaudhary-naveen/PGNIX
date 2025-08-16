048cec444dffa3f7a5d6ac71f4b3e2d3690693b7
Naveen Chaudhary <98143534+chaudhary-naveen@users.noreply.github.com>
2025-08-16 13:09:39 +0530

    vercel deployment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const connectdb = require("./config/connectdb.js");
+const serverless = require('serverless-http');
 // const dummy_routes = require("./routes/dummyroutes.js");
 
 const cookieParser = require("cookie-parser");
@@ -39,3 +40,4 @@ const server = app.listen(port, () => {
 });
 
 module.exports = app;
+module.exports.handler = serverless(app);
